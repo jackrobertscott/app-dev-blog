dbf6152f73f0c3fe5bb761181c936f6aab36758e
Jack Scott <jack.rob.scott@gmail.com>
2015-11-26 21:16:51 +0800

    put inject function back but add todo for integration with gulp-useref

--- gulp/inject.js
+++ gulp/inject.js
@@ -34,6 +34,33 @@ gulp.task('inject:tmp', function() {
     .pipe(gulp.dest(config.paths.tmp));
 });
 
+// TODO use gulp-useref so that otherfiles may be inserted
+
 gulp.task('inject:build', function() {
-  // TODO: add gulp-useref
+  var js = gulp.src([
+    path.join(config.paths.dist, '**/*.js'),
+    '!' + path.join(config.paths.dist, 'vendor/*.js'),
+  ], {
+    read: false,
+  });
+  var css = gulp.src([
+    path.join(config.paths.dist, '**/*.css'),
+    '!' + path.join(config.paths.dist, 'vendor/*.css'),
+  ], {
+    read: false,
+  });
+  var vendor = gulp.src([
+    path.join(config.paths.dist, 'vendor/*.js'),
+    path.join(config.paths.dist, 'vendor/*.css'),
+  ], {
+    read: false,
+  });
+
+  return gulp.src(path.join(config.paths.dist, '**/*.html'))
+    .pipe(inject(js))
+    .pipe(inject(css))
+    .pipe(inject(vendor, {
+      name: 'bower',
+    }))
+    .pipe(gulp.dest(config.paths.dist));
 });
